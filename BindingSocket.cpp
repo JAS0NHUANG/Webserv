afb0fa731f481237002b234f067b4bbe8d1b596e
ifeelbored <ursula771023@gmail.com>
2022-10-25 15:28:00 +0200

    kque

--- BindingSocket.cpp
+++
@@ -1,9 +0,0 @@
-#include "BindingSocket.hpp"
-
-BindingSocket::BindingSocket(int domain, int service, int protocol, int port, u_long interface): SimpleSocket(domain, service, protocol, port, interface){
-    test_connection(connect_to_network(get_sock(), get_addr()));
-}
-int BindingSocket::connect_to_network(int sock, struct sockaddr_in addr) {
-    //const struct sockaddr *pt = addr
-    return bind(sock,((struct sockaddr *)&addr), sizeof(addr));
-}
